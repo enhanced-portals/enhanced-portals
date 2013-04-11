fd5c5c4247ebee3bad43f7ec13d982fa79bb46fa
ShadedDimension <ShadedDimensions@googlemail.com>
2013-04-11 15:03:10 +0100

    Added ComputerCraft API calls to CC blocks.

--- common/com/alz/enhancedportals/computercraft/tileentity/TileEntityDialHomeDeviceCC.java
+++ common/com/alz/enhancedportals/computercraft/tileentity/TileEntityDialHomeDeviceCC.java
@@ -1,8 +1,50 @@
 package com.alz.enhancedportals.computercraft.tileentity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.alz.enhancedportals.core.tileentity.TileEntityDialHomeDevice;
 
-public class TileEntityDialHomeDeviceCC extends TileEntityDialHomeDevice
-{
+import dan200.computer.api.IComputerAccess;
+import dan200.computer.api.IPeripheral;
 
+public class TileEntityDialHomeDeviceCC extends TileEntityDialHomeDevice implements IPeripheral
+{
+    List<IComputerAccess> computers = new ArrayList<IComputerAccess>();
+    
+    @Override
+    public String getType()
+    {
+        return "dialHomeDevice";
+    }
+
+    @Override
+    public String[] getMethodNames()
+    {
+        return new String[] { "activate", "deactivate", "selectEntry", "addEntry", "removeEntry" };
+    }
+
+    @Override
+    public Object[] callMethod(IComputerAccess computer, int method, Object[] arguments) throws Exception
+    {
+        return null;
+    }
+
+    @Override
+    public boolean canAttachToSide(int side)
+    {
+        return true;
+    }
+
+    @Override
+    public void attach(IComputerAccess computer)
+    {
+        computers.add(computer);
+    }
+
+    @Override
+    public void detach(IComputerAccess computer)
+    {
+        computers.remove(computer);
+    }
 }
--- common/com/alz/enhancedportals/computercraft/tileentity/TileEntityPortalModifierCC.java
+++ common/com/alz/enhancedportals/computercraft/tileentity/TileEntityPortalModifierCC.java
@@ -1,8 +1,52 @@
 package com.alz.enhancedportals.computercraft.tileentity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.alz.enhancedportals.core.tileentity.TileEntityPortalModifier;
 
-public class TileEntityPortalModifierCC extends TileEntityPortalModifier
-{
+import dan200.computer.api.IComputerAccess;
+import dan200.computer.api.IPeripheral;
 
+public class TileEntityPortalModifierCC extends TileEntityPortalModifier implements IPeripheral
+{
+    List<IComputerAccess> computers = new ArrayList<IComputerAccess>();
+    
+    @Override
+    public String getType()
+    {
+        return "portalModifier";
+    }
+
+    @Override
+    public String[] getMethodNames()
+    {
+        return new String[] { "activate", "deactivate", "getNetwork", "setNetwork", "getTexture", "setTexture" };
+    }
+
+    @Override
+    public Object[] callMethod(IComputerAccess computer, int method, Object[] arguments) throws Exception
+    {
+        return null;
+    }
+
+    @Override
+    public boolean canAttachToSide(int side)
+    {
+        // only if has upgrade
+        
+        return true;
+    }
+
+    @Override
+    public void attach(IComputerAccess computer)
+    {
+        computers.add(computer);
+    }
+
+    @Override
+    public void detach(IComputerAccess computer)
+    {
+        computers.remove(computer);
+    }
 }
